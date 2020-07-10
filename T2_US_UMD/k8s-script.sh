6d3a8ac7475a6f331b480109889b2da462c08f67
Xi Yang <xiyang@es.net>
2020-07-10 15:00:49 -0400

    k8s script and yaml

---
+++ T2_US_UMD/k8s-script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+### Run on 180-131.research.maxgigapop.net (a.k.a. master-131)
+# config for FE
+kubectl create secret generic sense-httpdprivkey --from-file=httpdprivkey=/etc/letsencrypt/live/180-131.research.maxgigapop.net/privkey.pem
+kubectl create secret generic sense-httpdcert --from-file=httpdcert=/etc/letsencrypt/live/180-131.research.maxgigapop.net/cert.pem
+kubectl create secret generic sense-hostkey --from-file=hostkey=/etc/letsencrypt/live/180-131.research.maxgigapop.net/privkey.pem
+kubectl create secret generic sense-hostcert --from-file=hostcert=/etc/letsencrypt/live/180-131.research.maxgigapop.net/cert.pem
+kubectl create secret generic sense-httpdfullchain --from-file=httpdfullchain=/etc/letsencrypt/live/180-131.research.maxgigapop.net/fullchain.pem
+kubectl create configmap sense-siterm-fe-yaml --from-file=sense-siterm-fe=/etc/dtnrm.yaml
+
+# config for Agents
+kubectl create secret generic sense-agent-hostcert --from-file=agent-hostcert=/etc/letsencrypt/live/180-131.research.maxgigapop.net/cert.pem
+kubectl create secret generic sense-agent-hostkey --from-file=agent-hostkey=/etc/letsencrypt/live/180-131.research.maxgigapop.net/privkey.pem
+kubectl create configmap sense-siterm-agent01-yaml --from-file=sense-siterm-agent=/etc/dtnrm-agent01.yaml
+kubectl create configmap sense-siterm-agent02-yaml --from-file=sense-siterm-agent=/etc/dtnrm-agent02.yaml
+
+# deploy site-rm
+kubectl apply -f k8s-siterm-umd.yaml
