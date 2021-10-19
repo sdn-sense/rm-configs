c7deecdbd8c3cad41d0765176640c9171d611917
xi-yang <xiyang@es.net>
2021-10-19 10:09:51 -0400

    Update k8s-script.sh

--- T3_US_Starlight/K8s/k8s-script.sh
+++ T3_US_Starlight/K8s/k8s-script.sh
@@ -6,17 +6,17 @@
 export MASTER_NODE_PATH=/opt/siterm/installers/fe-docker//conf/etc/httpd/certs
 
 # config for FE
-kubectl create secret generic sense-httpdprivkey --from-file=httpdprivkey=/MASTER_NODE_PATH/privkey.pem
-kubectl create secret generic sense-httpdcert --from-file=httpdcert=/MASTER_NODE_PATH/cert.pem
-kubectl create secret generic sense-hostkey --from-file=hostkey=/MASTER_NODE_PATH/privkey.pem
-kubectl create secret generic sense-hostcert --from-file=hostcert=/MASTER_NODE_PATH/cert.pem
-kubectl create secret generic sense-httpdfullchain --from-file=httpdfullchain=/MASTER_NODE_PATH/fullchain.pem
-kubectl create secret generic sense-environment --from-file=environment=/MASTER_NODE_PATH/siterm-mariadb
+kubectl create secret generic sense-httpdprivkey --from-file=httpdprivkey=$MASTER_NODE_PATH/privkey.pem
+kubectl create secret generic sense-httpdcert --from-file=httpdcert=$MASTER_NODE_PATH/cert.pem
+kubectl create secret generic sense-hostkey --from-file=hostkey=$MASTER_NODE_PATH/privkey.pem
+kubectl create secret generic sense-hostcert --from-file=hostcert=$MASTER_NODE_PATH/cert.pem
+kubectl create secret generic sense-httpdfullchain --from-file=httpdfullchain=$MASTER_NODE_PATH/fullchain.pem
+kubectl create secret generic sense-environment --from-file=environment=$MASTER_NODE_PATH/siterm-mariadb
 kubectl create configmap sense-siterm-fe-yaml --from-file=sense-siterm-fe=/etc/dtnrm.yaml
 
 # config for Agents
-kubectl create secret generic sense-agent-hostcert --from-file=agent-hostcert=/MASTER_NODE_PATH/cert.pem
-kubectl create secret generic sense-agent-hostkey --from-file=agent-hostkey=/MASTER_NODE_PATH/privkey.pem
+kubectl create secret generic sense-agent-hostcert --from-file=agent-hostcert=$MASTER_NODE_PATH/cert.pem
+kubectl create secret generic sense-agent-hostkey --from-file=agent-hostkey=$MASTER_NODE_PATH/privkey.pem
 kubectl create configmap sense-siterm-agent01-yaml --from-file=sense-siterm-agent=/etc/dtnrm-agent01.yaml
 
 # deploy site-rm
