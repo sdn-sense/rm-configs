83390687d4d2371aeb2e486862972ff7b1ce8d72
John Graham <jjgraham@ucsd.edu>
2022-03-15 22:17:26 -0700

    Update k8s-script.sh
    clean paths

--- T2_US_UH_GOREX/K8s/k8s-script.sh
+++ T2_US_UH_GOREX/K8s/k8s-script.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-## Run on K8s master node
-### Download /etc/dtnrm-agent01.yaml and /etc/dtnrm-agent01.yaml from https://github.com/sdn-sense/rm-configs/blob/master/T3_US_HAWAII/K8s 
-### Generate / obtain privkey and cert etc. secret files and place them on the master node under /MASTER_NODE_PATH/.
+### Generate / obtain privkey and cert etc. secret files and place them in K8s folder.
 
 # config for FE
-kubectl create secret generic sense-httpdprivkey --from-file=httpdprivkey=/MASTER_NODE_PATH/privkey.pem -n hawaii-opennsa
-kubectl create secret generic sense-httpdcert --from-file=httpdcert=/MASTER_NODE_PATH/cert.pem -n hawaii-opennsa
-kubectl create secret generic sense-hostkey --from-file=hostkey=/MASTER_NODE_PATH/privkey.pem -n hawaii-opennsa
-kubectl create secret generic sense-hostcert --from-file=hostcert=/MASTER_NODE_PATH/cert.pem -n hawaii-opennsa
-kubectl create secret generic sense-httpdfullchain --from-file=httpdfullchain=/MASTER_NODE_PATH/fullchain.pem -n hawaii-opennsa
-kubectl create secret generic sense-environment --from-file=environment=/MASTER_NODE_PATH/siterm-mariadb -n hawaii-opennsa
-kubectl create configmap sense-siterm-fe-yaml --from-file=sense-siterm-fe=/etc/dtnrm.yaml -n hawaii-opennsa
+kubectl create secret generic sense-httpdprivkey --from-file=httpdprivkey=privkey.pem -n hawaii-opennsa
+kubectl create secret generic sense-httpdcert --from-file=httpdcert=cert.pem -n hawaii-opennsa
+kubectl create secret generic sense-hostkey --from-file=hostkey=privkey.pem -n hawaii-opennsa
+kubectl create secret generic sense-hostcert --from-file=hostcert=cert.pem -n hawaii-opennsa
+kubectl create secret generic sense-httpdfullchain --from-file=httpdfullchain=fullchain.pem -n hawaii-opennsa
+kubectl create secret generic sense-environment --from-file=environment=siterm-mariadb -n hawaii-opennsa
+kubectl create configmap sense-siterm-fe-yaml --from-file=sense-siterm-fe=dtnrm.yaml -n hawaii-opennsa
 
 # config for Agents
-kubectl create secret generic sense-agent-hostcert --from-file=agent-hostcert=/MASTER_NODE_PATH/cert.pem -n hawaii-opennsa
-kubectl create secret generic sense-agent-hostkey --from-file=agent-hostkey=/MASTER_NODE_PATH/privkey.pem -n hawaii-opennsa
-kubectl create configmap sense-siterm-agent01-yaml --from-file=sense-siterm-agent=/etc/dtnrm-agent01.yaml -n hawaii-opennsa
+kubectl create secret generic sense-agent-hostcert --from-file=agent-hostcert=cert.pem -n hawaii-opennsa
+kubectl create secret generic sense-agent-hostkey --from-file=agent-hostkey=privkey.pem -n hawaii-opennsa
+kubectl create configmap sense-siterm-agent01-yaml --from-file=sense-siterm-agent=dtnrm-agent01.yaml -n hawaii-opennsa
+kubectl create configmap sense-siterm-agent02-yaml --from-file=sense-siterm-agent=dtnrm-agent02.yaml -n hawaii-opennsa
+kubectl create configmap sense-siterm-agent03-yaml --from-file=sense-siterm-agent=dtnrm-agent03.yaml -n hawaii-opennsa
 
 # deploy site-rm
 kubectl apply -f k8s-siterm.yaml -n hawaii-opennsa
