c6e29f3f4f94561a0251772398bf0fa4d5a2a210
John Graham <jjgraham@ucsd.edu>
2022-03-15 21:48:39 -0700

    add namespace

--- T2_US_UH_GOREX/K8s/k8s-script.sh
+++ T2_US_UH_GOREX/K8s/k8s-script.sh
@@ -4,18 +4,18 @@
 ### Generate / obtain privkey and cert etc. secret files and place them on the master node under /MASTER_NODE_PATH/.
 
 # config for FE
-kubectl create secret generic sense-httpdprivkey --from-file=httpdprivkey=/MASTER_NODE_PATH/privkey.pem
-kubectl create secret generic sense-httpdcert --from-file=httpdcert=/MASTER_NODE_PATH/cert.pem
-kubectl create secret generic sense-hostkey --from-file=hostkey=/MASTER_NODE_PATH/privkey.pem
-kubectl create secret generic sense-hostcert --from-file=hostcert=/MASTER_NODE_PATH/cert.pem
-kubectl create secret generic sense-httpdfullchain --from-file=httpdfullchain=/MASTER_NODE_PATH/fullchain.pem
-kubectl create secret generic sense-environment --from-file=environment=/MASTER_NODE_PATH/siterm-mariadb
-kubectl create configmap sense-siterm-fe-yaml --from-file=sense-siterm-fe=/etc/dtnrm.yaml
+kubectl create secret generic sense-httpdprivkey --from-file=httpdprivkey=/MASTER_NODE_PATH/privkey.pem -n hawaii-opennsa
+kubectl create secret generic sense-httpdcert --from-file=httpdcert=/MASTER_NODE_PATH/cert.pem -n hawaii-opennsa
+kubectl create secret generic sense-hostkey --from-file=hostkey=/MASTER_NODE_PATH/privkey.pem -n hawaii-opennsa
+kubectl create secret generic sense-hostcert --from-file=hostcert=/MASTER_NODE_PATH/cert.pem -n hawaii-opennsa
+kubectl create secret generic sense-httpdfullchain --from-file=httpdfullchain=/MASTER_NODE_PATH/fullchain.pem -n hawaii-opennsa
+kubectl create secret generic sense-environment --from-file=environment=/MASTER_NODE_PATH/siterm-mariadb -n hawaii-opennsa
+kubectl create configmap sense-siterm-fe-yaml --from-file=sense-siterm-fe=/etc/dtnrm.yaml -n hawaii-opennsa
 
 # config for Agents
-kubectl create secret generic sense-agent-hostcert --from-file=agent-hostcert=/MASTER_NODE_PATH/cert.pem
-kubectl create secret generic sense-agent-hostkey --from-file=agent-hostkey=/MASTER_NODE_PATH/privkey.pem
-kubectl create configmap sense-siterm-agent01-yaml --from-file=sense-siterm-agent=/etc/dtnrm-agent01.yaml
+kubectl create secret generic sense-agent-hostcert --from-file=agent-hostcert=/MASTER_NODE_PATH/cert.pem -n hawaii-opennsa
+kubectl create secret generic sense-agent-hostkey --from-file=agent-hostkey=/MASTER_NODE_PATH/privkey.pem -n hawaii-opennsa
+kubectl create configmap sense-siterm-agent01-yaml --from-file=sense-siterm-agent=/etc/dtnrm-agent01.yaml -n hawaii-opennsa
 
 # deploy site-rm
-kubectl apply -f k8s-siterm.yaml
+kubectl apply -f k8s-siterm.yaml -n hawaii-opennsa
